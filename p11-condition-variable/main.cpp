a2b1f6be92670f2ca1d60baca66eba0e2c545262
toothpickstirvat <toothpickstirvat@gmail.com>
2026-06-09 14:54:04 +0800

    feat: condition variable

---
+++ p11-condition-variable/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+using namespace std;
+
+std::condition_variable cv;
+std::mutex m;
+long balance = 0;
+
+void addMoney(int money) {
+    std::lock_guard<mutex> lg(m);
+    balance += money;
+    cout << "Amount added current balance: " << balance << endl;
+    cv.notify_one();
+}
+
+void withdrawMoney(int money) {
+    std::unique_lock<mutex> ul(m); // 加锁
+    cv.wait(ul, []{ return balance != 0; }); // 等待，如果条件不成立，释放锁
+    if (balance >= money) {
+        balance -= money;
+        cout << "Amount deducted: " << money << endl;
+    } else {
+        cout << "Amount can't be deducted, current balance is less than " << money << endl;
+    }
+    cout << "Current balance is: " << balance << endl;
+}
+
+int main() {
+    std::thread t1(withdrawMoney, 500);
+    std::thread t2(addMoney, 500);
+    t1.join();
+    t2.join();
+
+    return 0;
+}
