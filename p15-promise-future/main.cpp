ee1dd5bac31d2bd999eb209f9049611e3c53a1d5
toothpickstirvat <toothpickstirvat@gmail.com>
2026-06-10 22:05:42 +0800

    feat: promise, future

---
+++ p15-promise-future/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <algorithm>
+#include <future>
+using namespace std;
+using namespace std::chrono;
+typedef unsigned long long ull;
+
+void findOdd(std::promise<ull>&& oddSumPromise, ull start, ull end) {
+    ull oddSum = 0;
+    for (ull i = start; i <= end; ++i) {
+        if (i & 1) {
+            oddSum += i;
+        }
+    }
+    oddSumPromise.set_value(oddSum);
+}
+
+int main() {
+    ull start = 0, end = 1900000000;
+
+    // 创建promise对象
+    std::promise<ull> oddSum; // 奇数和
+    // 用promise创建future对象
+    std::future<ull> oddSumFuture = oddSum.get_future();
+
+    cout << "Thread created!!" << endl;
+    std::thread t1(findOdd, std::move(oddSum), start, end);
+
+    cout << "Waiting for result!!" << endl;
+    cout << "oddSum: " << oddSumFuture.get() << endl;
+    cout << "Completed!!" << endl;
+
+    t1.join();
+
+    return 0;
+}
+
+
