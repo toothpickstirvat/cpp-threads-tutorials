9660514ad083b8bac2ad0a059e4b7179e3d20bb5
toothpickstirvat <toothpickstirvat@gmail.com>
2026-06-10 21:28:40 +0800

    feat: thread sync

---
+++ p13-thread-synchronization/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+using namespace std;
+
+long long balance = 0;
+std::mutex m;
+
+void addMoney(long long val) {
+    m.lock();
+    balance += val;
+    m.unlock();
+}
+
+int main() {
+    std::thread t1(addMoney, 100);
+    std::thread t2(addMoney, 200);
+
+    t1.join();
+    t2.join();
+
+    cout << "Balance: " << balance << endl;
+
+    return 0;
+}
